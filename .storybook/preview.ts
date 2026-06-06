cfb1cce94476a91ad51fdc83b36d6cdf44d8b957
tanishraj <learn.tanishraj@gmail.com>
2026-06-06 06:12:47 +0400

    feat: updated globals css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/react';
 import { withThemeByDataAttribute } from '@storybook/addon-themes';
 
-import '../src/theme/primary.css';
+import '../src/styles/globals.css';
 
 const preview: Preview = {
   parameters: {
