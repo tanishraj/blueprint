0659bd86a01879ddab4912bb65fe2bd0eb92005e
tanishraj <learn.tanishraj@gmail.com>
2026-06-10 17:52:29 +0400

    fix: Button storybook updated

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -292,24 +292,24 @@ export const ButtonInverted: Story = {
     const states = [{ label: 'Inverted', props: { inverted: true } }] as const;
 
     return (
-      <div className='flex flex-col gap-10 bg-default-pressed-inverted text-default-inverted p-5'>
+      <div className='flex flex-col gap-1 text-default p-5'>
         {states.map(({ label, props }, index) => (
           <section key={label} className='flex flex-col gap-5'>
             <div className={variantGridClass}>
-              <span className='text-base text-default-inverted font-bold'>
+              <span className='text-base text-default font-bold'>
                 Appearance
               </span>
               {variants.map(variant => (
                 <span
                   key={variant}
-                  className='text-sm text-default-inverted font-bold capitalize'
+                  className='text-sm text-default font-bold capitalize'
                 >
                   {variant}
                 </span>
               ))}
               {appearances.map(appearance => (
                 <Fragment key={`${label}-${appearance}`}>
-                  <h4 className='text-base text-default-inverted font-bold capitalize'>
+                  <h4 className='text-base text-default font-bold capitalize'>
                     {appearance}
                   </h4>
                   {variants.map(variant => (
