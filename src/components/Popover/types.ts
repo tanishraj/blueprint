a6a1056006c1740b0c40a61143821e57c573468e
Tanish Raj <tanish@liquidity-capital.com>
2026-06-26 14:26:55 +0400

    fix: Popover component

--- src/components/Popover/types.ts
+++ src/components/Popover/types.ts
@@ -1,7 +1,7 @@
 import type { HTMLAttributes, ReactNode } from 'react';
 import type { VariantProps } from 'class-variance-authority';
 
-import type { RemoveNull } from '@/utils';
+import type { RemoveNull } from '@/utils/types';
 
 import { popoverIconStyles } from './Popover.styles';
 
