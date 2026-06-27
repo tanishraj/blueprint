a7cc8c0a280e1e2d758157452ebbd77ff57c411b
tanishraj <learn.tanishraj@gmail.com>
2026-06-28 03:04:26 +0400

    fix: storybook theme

--- vite.config.mjs
+++ vite.config.mjs
@@ -71,5 +71,8 @@ export default defineConfig({
       formats: ['es', 'cjs'],
       fileName: format => `index.${format}.js`,
     },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
   },
 });
